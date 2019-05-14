2d0db57ade261855b2d9b4c6199f6f04f2022df0
Trang Tran Thi Ngoc <tn-trang.tran@outlook.com.vn>
2019-05-14 19:11:55 +0700

    [Trang]: Merge code

--- DocumentManagementSystem.WebApp/Content/rootApp.js
+++ DocumentManagementSystem.WebApp/Content/rootApp.js
@@ -1,5 +1,8 @@
 var rootApp;
 (function (rootApp) {
-    var myApp = angular.module('rootApp', []);
+    'use strict';
+    angular.module('directiveModule', []).directive('addDocument', rootApp.DocumentDirective.Factory());
+    angular.module('controllerModule', ['directiveModule']).controller('DocumentController', rootApp.DocumentController).controller('TableController', rootApp.TableController);
+    angular.module('rootApp', ['controllerModule', 'directiveModule']);
 })(rootApp || (rootApp = {}));
 //# sourceMappingURL=rootApp.js.map
