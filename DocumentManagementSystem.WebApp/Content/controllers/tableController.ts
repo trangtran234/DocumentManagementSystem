fabe3d191b2431c5a62699b3ccc82e840417a52e
Trang Tran Thi Ngoc <tn-trang.tran@outlook.com.vn>
2019-05-15 17:59:37 +0700

    [Trang]: Publish event and display child folders

--- DocumentManagementSystem.WebApp/Content/controllers/tableController.ts
+++ DocumentManagementSystem.WebApp/Content/controllers/tableController.ts
@@ -5,60 +5,32 @@
 
     interface IDocumentScope extends ng.IScope {
         documents: Document[];
-        documentsByFolderId: Document[];
+        getDocumentByFolderId: (id) => void;
     }
 
     export class TableController  {
-
         static $inject = ['$http', '$scope'];
 
-        constructor(private $http: ng.IHttpService, private $scope: IDocumentScope) {
-            this.getDocumentByFolderId()
+        constructor(private $http: ng.IHttpService, private $scope: IDocumentScope, ) {
+            this.getDocument()
                 .then((response: ng.IHttpPromiseCallbackArg<Document[]>): Document[] => {
                     return <Document[]>response.data;
                 })
                 .then((response: Document[]) => {
                     $scope.documents = response;
-            });
+                });
+            $scope.getDocumentByFolderId = this.getDocumentByFolderId;
         }
+        
 
-        getDocumentByFolderId(): ng.IPromise<ng.IHttpResponse<Document[]>>{
+        getDocument(): ng.IPromise<ng.IHttpResponse<Document[]>>{
             return this.$http.get<Document[]>('/api/documents/DocumentByFolderId/3');
         }
 
-        
-
-
+        getDocumentByFolderId(id) {
+            console.log("Click on document");
+            console.log("Id:" + id);
+            console.log("/api/documents/DocumentByFolderId/" + id);
+        }
     }
-
-    //export class TableController {
-    //    public Documents: Document[];
-
-    //    static $inject = ['$scope', '$http']
-
-    //    constructor(private $http: ng.IHttpService) {
-    //        this.getDocuments();
-    //    }
-
-    //    getDocuments(): Document[] {
-    //        return this.$http.get('/api/documents/documentDTO')
-    //            .then((response: ng.IHttpPromiseCallbackArg<Document[]>): Document[] => {
-    //                return <Document[]>response.data;
-    //            });
-    //    }
-    //}
-
-    //export class TableController
-    //{
-    //    public scope;
-    //    static $inject = ['$scope'];
-    //    public documentDTO = ['Catherine Grant', 'Monica Grant',
-    //        'Christopher Grant', 'Jennifer Grant'
-    //    ];
-
-    //    constructor(private $scope: ng.IScope) {
-    //        this.scope = $scope;
-    //        this.scope.Documents = this.documentDTO;
-    //    }
-    //}
 }
