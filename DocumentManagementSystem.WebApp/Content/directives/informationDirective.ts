36d08aa1b759edb543616815b52f46919b7ee80a
Trang Tran Thi Ngoc <tn-trang.tran@outlook.com.vn>
2019-05-22 11:58:21 +0700

    Revert "Revert "[Trang]: update view""
    This reverts commit 4587b21854c0e8342abf0bed6bd7d536b2831433.

--- DocumentManagementSystem.WebApp/Content/directives/informationDirective.ts
+++ DocumentManagementSystem.WebApp/Content/directives/informationDirective.ts
@@ -1,15 +1,30 @@
 ﻿module rootApp {
     'use strict';
+
+    interface IMyDocumentScope extends ng.IScope {
+        document: Document;
+    }
+
     export class InformationDirective implements ng.IDirective {
         public restrict: string = "E";
-        public templateUrl: string = '/Content/directives/informationDocumentTag.html';
-        public scope = {
-            documentInfo: '=info'
-        };
+        public templateUrl: string = '/Content/directives/informationDocument.html';
+        //public scope = {
+        //    documentInfo: '=info'
+        //};
         constructor() { }
 
         public static Factory(): ng.IDirectiveFactory {
             return () => new InformationDirective();
         }
+
+        public link: ng.IDirectiveLinkFn = (
+            scope: IMyDocumentScope,
+            element: ng.IAugmentedJQuery,
+            attributes: ng.IAttributes
+        ) => {
+            scope.$on('rootScope:documentInfo', function (event, data) {
+                scope.document = data;
+            });
+        };
     }
 }
