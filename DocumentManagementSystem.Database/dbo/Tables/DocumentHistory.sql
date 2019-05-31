4974d7c655199e959cb2ca01182f904249c4d80b
Trang Tran Thi Ngoc <tn-trang.tran@outlook.com.vn>
2019-05-31 09:42:26 +0700

    [Trang]: modal angularjs

---
+++ DocumentManagementSystem.Database/dbo/Tables/DocumentHistory.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[DocumentHistory]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL,
+	[DocumentId] INT NOT NULL,
+	[ActionId] INT NOT NULL,
+	[UserId] INT NOT NULL,
+	[Date] DATETIME NOT NULL
+
+	PRIMARY KEY CLUSTERED ([Id] ASC),
+	FOREIGN KEY ([DocumentId]) REFERENCES [dbo].[Document] ([Id]),
+	FOREIGN KEY ([UserId]) REFERENCES [dbo].[Account] ([Id])
+)
