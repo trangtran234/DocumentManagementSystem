bbe62de46ffc4a203d9e189cb46ce642b8cf3ebd
Trang Tran Thi Ngoc <tn-trang.tran@outlook.com.vn>
2019-06-03 18:47:08 +0700

    [Trang]: Stored Document History

---
+++ DocumentManagementSystem.Repository/Interface/IDocumentHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using DocumentManagementSystem.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManagementSystem.Repository.Interface
+{
+    public interface IDocumentHistoryRepository
+    {
+        bool AddDocumentHistory(Document document, Helper.HistoryAction actionEvent);
+        List<DocumentHistory> GetDocumentHistories();
+    }
+}
