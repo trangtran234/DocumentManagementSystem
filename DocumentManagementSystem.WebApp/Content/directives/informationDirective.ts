34800a17822e46a56971a5e0fdd909613cc00815
Trang Tran Thi Ngoc <tn-trang.tran@outlook.com.vn>
2019-06-04 19:34:57 +0700

    [Trang]: updated Document history view

--- DocumentManagementSystem.WebApp/Content/directives/informationDirective.ts
+++ DocumentManagementSystem.WebApp/Content/directives/informationDirective.ts
@@ -9,17 +9,18 @@
         isVisible: boolean;
         documentHistories;
         messages;
+        getDocumentHistories: () => void;
     }
 
     export class InformationDirective implements ng.IDirective {
         public restrict: string = "E";
         public templateUrl: string = '/Content/directives/informationDocument.html';
 
-        constructor(private $http: ng.IHttpService, private $sce: ng.ISCEService) { }
+        constructor(private $http: ng.IHttpService, private $sce: ng.ISCEService, private $filter: ng.IFilterService) { }
 
         public static Factory(): ng.IDirectiveFactory {
-            const directive = ($http: ng.IHttpService, $sce: ng.ISCEService) => new InformationDirective($http, $sce);
-            directive.$inject = ['$http', '$sce'];
+            const directive = ($http: ng.IHttpService, $sce: ng.ISCEService, $filter: ng.IFilterService) => new InformationDirective($http, $sce, $filter);
+            directive.$inject = ['$http', '$sce', '$filter'];
             return directive;
         }
 
@@ -38,63 +39,48 @@
 
             var http = this.$http;
             var sce = this.$sce;
+            var filter = this.$filter;
             scope.documentHistories = new Array<DocumentHistory>();
             scope.messages = new Array<string>();
-
-            http.get<DocumentHistory[]>('/api/documents/DocumentHistories')
-                .then((response: ng.IHttpPromiseCallbackArg<DocumentHistory[]>) => {
-                    angular.forEach(response.data, function (value, key) {
-                        var docHistory = new DocumentHistory(value.id, value.documentId, value.actionEvent, value.account, value.date);
-
-                        var msg: string;
-                        switch (docHistory.actionEvent) {
-                            case 1:
-                                msg = sce.trustAsHtml('Created by <a href="">' + docHistory.account.username + '</a> at ' + docHistory.date);
-                                break;
-                            case 2:
-                                msg = sce.trustAsHtml('Updated by <a href="">' + docHistory.account.username + '</a> at ' + docHistory.date);
-                                break;
-                            case 3:
-                                msg = sce.trustAsHtml('Deleted by <a href="">' + docHistory.account.username + '</a> at ' + docHistory.date);
-                                break;
-                        }
-                        scope.messages.push(msg);
-
-                        scope.documentHistories.push(docHistory);
-                    })
-                });
+            
 
             scope.$on('history:sucessed', function (event, data) {
                 var actionEvent = data;
                 if (actionEvent !== null) {
-                    http.get<DocumentHistory[]>('/api/documents/DocumentHistories')
-                        .then((response: ng.IHttpPromiseCallbackArg<DocumentHistory[]>) => {
-                            scope.documentHistories = [];
-
-                            scope.messages = [];
-
-                            angular.forEach(response.data, function (value, key) {
-                                var docHistory = new DocumentHistory(value.id, value.documentId, value.actionEvent, value.account, value.date);
-
-                                var msg: string;
-                                switch (docHistory.actionEvent) {
-                                    case 1:
-                                        msg = sce.trustAsHtml('Created by <a href="">' + docHistory.account.username + '</a> at ' + docHistory.date);
-                                        break;
-                                    case 2:
-                                        msg = sce.trustAsHtml('Updated by <a href="">' + docHistory.account.username + '</a> at ' + docHistory.date);
-                                        break;
-                                    case 3:
-                                        msg = sce.trustAsHtml('Deleted by <a href="">' + docHistory.account.username + '</a> at ' + docHistory.date);
-                                        break;
-                                }
-                                scope.messages.push(msg);
-
-                                scope.documentHistories.push(docHistory);
-                            })
-                        });
+                    scope.getDocumentHistories();
                 }
             });
+
+            scope.getDocumentHistories = () => {
+                http.get<DocumentHistory[]>('/api/documents/DocumentHistories')
+                    .then((response: ng.IHttpPromiseCallbackArg<DocumentHistory[]>) => {
+                        scope.documentHistories = [];
+                        scope.messages = [];
+                        angular.forEach(response.data, function (value, key) {
+                            var docHistory = new DocumentHistory(value.id, value.documentId, value.actionEvent, value.account, value.date);
+                            
+                            var msg: string;
+                            var date = new Date(docHistory.date + "");
+                            var dateStr = filter('date')(date, 'yyyy-MM-dd HH:mm:ss');
+                            switch (docHistory.actionEvent) {
+                                case 1:
+                                    msg = sce.trustAsHtml('Created by <a href="">' + docHistory.account.username + '</a> at ' + dateStr);
+                                    break;
+                                case 2:
+                                    msg = sce.trustAsHtml('Updated by <a href="">' + docHistory.account.username + '</a> at ' + dateStr);
+                                    break;
+                                case 3:
+                                    msg = sce.trustAsHtml('Deleted by <a href="">' + docHistory.account.username + '</a> at ' + dateStr);
+                                    break;
+                            }
+                            scope.messages.push(msg);
+
+                            scope.documentHistories.push(docHistory);
+                        })
+                    });
+            }
+
+            scope.getDocumentHistories();
         };
     }
 }
